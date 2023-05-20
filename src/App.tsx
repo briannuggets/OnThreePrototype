849aea8b14ae1d9beffd9776cb2efb0fd884d3d0
Brian Nguyen <briannguyen.sd@gmail.com>
2023-05-20 15:05:30 -0400

    Incorporate react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/general/Nav";
 import Home from "./components/page/Home";
+import Work from "./components/page/Work";
+import About from "./components/page/About";
+import Contact from "./components/page/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
