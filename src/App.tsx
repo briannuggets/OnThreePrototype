a3e1fe32a115db89bc2c384a461dde4cceb89b8b
Brian Nguyen <briannguyen.sd@gmail.com>
2023-05-24 15:40:47 -0400

    Add smart mouse trailer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import { useRef } from "react";
+import { MdArrowOutward } from "react-icons/md";
 import Nav from "./components/general/Nav";
 import Home from "./components/page/Home";
 import Work from "./components/page/Work";
@@ -6,8 +8,37 @@ import About from "./components/page/About";
 import Contact from "./components/page/Contact";
 
 function App() {
-  window.onbeforeunload = function () {
+  window.onbeforeunload = () => {
     window.scrollTo(0, 0);
+  };
+
+  const trailerRef = useRef<HTMLDivElement>(null);
+  window.onmousemove = (e) => {
+    if (trailerRef.current === null) {
+      return;
+    }
+    if (e.target === null) {
+      return;
+    }
+
+    const interactable = (e.target as Element).closest(".interactable");
+
+    if (interactable !== null) {
+      trailerRef.current.children[0].classList.add("visible");
+    } else {
+      trailerRef.current.children[0].classList.remove("visible");
+    }
+
+    trailerRef.current.animate(
+      {
+        transform: `translate(${
+          e.clientX - trailerRef.current.offsetWidth
+        }px, ${e.clientY - trailerRef.current.offsetHeight}px) scale(${
+          interactable !== null ? 8 : 1
+        })`,
+      },
+      { duration: 200, fill: "forwards" }
+    );
   };
 
   return (
@@ -19,6 +50,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <div id="mouse-trailer" ref={trailerRef}>
+        <MdArrowOutward size={5} />
+      </div>
     </div>
   );
 }
